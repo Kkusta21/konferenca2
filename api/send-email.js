e46d2edb14ec24536cd539a4d61706d657b1ee84
Kkusta21 <104655550+Kkusta21@users.noreply.github.com>
2026-04-20 23:22:16 +0200

    Change logo URL from PNG to JPG

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-const LOGO_URL   = 'https://raw.githubusercontent.com/Kkusta21/konferenca2/main/assets/logo.png';
+const LOGO_URL   = 'https://raw.githubusercontent.com/Kkusta21/konferenca2/main/assets/logo.jpg';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
